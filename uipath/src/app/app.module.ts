f57835955fa0157360916e09b6be6ab785d1ce30
andreirogobete <rogobete.andrei@gmail.com>
2023-02-13 16:34:56 +0200

    update app module

--- uipath/src/app/app.module.ts
+++ uipath/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,15 +10,18 @@ import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { GridEntryDirective } from './components/grid/directives/grid-entry/grid-entry.directive';
+import { ProgressComponent } from './components/progress/progress.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GridComponent,
-    GridEntryDirective
+    GridEntryDirective,
+    ProgressComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     CommonModule,
     HttpClientModule,
     AppRoutingModule,
